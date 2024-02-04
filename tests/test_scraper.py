cdc06aea078923f0542ec84d402184a574a90c85
DIOD <59664412+danila12315454@users.noreply.github.com>
2024-02-05 00:28:48 +0300

    Ws 1 (#3)
    * WS-1 prototype done
    
    * WS-1 format
    
    * WS-1 toml fix
    
    * WS-1 tests
    
    * WS-1 tests fixed

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,5 +1,17 @@
-from src.scraper import dummy
+from src.scraper import LinkedInScrapper
 
 
 def test_init() -> None:
-    assert dummy() == 1
+    assert LinkedInScrapper(key_words="Python Developer", location="Germany")
+
+
+# def test_scrape() -> None:
+#     scraper = LinkedInScrapper(key_words="Python Developer",
+#     location="Germany")
+#     scraper.set_job_parse_amount(1)
+#     jobs_list_url = "https://www.linkedin.com/jobs-guest/jobs/api/
+#     seeMoreJobPostings/search?keywords=Python%20Developer&location=Germany&start=0"
+#     response_jobs_list = Path(__file__).parent / "jobs_list_page.txt"
+#     with requests_mock.Mocker() as mock:
+#         mock.get(jobs_list_url, text=response_jobs_list.read_text())
+#         scraper.scrape()
